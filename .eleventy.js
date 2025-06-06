ddc96bbd94ad51edaf9fc07fb13c3cce5618910a
Kraaketing <nedogedell@protonmail.com>
2025-06-06 17:17:45 +0100

    Another another  try

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function (eleventyConfig) {
 eleventyConfig.addPassthroughCopy({ "src/admin": "admin" });
+eleventyConfig.addPassthroughCopy("src/_redirects");
+
 
   return {
     dir: {
