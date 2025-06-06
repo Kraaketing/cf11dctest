fe1cfad579e7e19d3d0ed76857cb698ab5c87393
Kraaketing <nedogedell@protonmail.com>
2025-06-06 20:49:38 +0100

    trying yet another thing

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ "src/static/admin": "admin" });
-  eleventyConfig.addPassthroughCopy({ "src/static/_headers": "_headers" });
+eleventyConfig.addPassthroughCopy("src/static");
 
 
   return {
